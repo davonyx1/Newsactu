7a21f86eac469dc0a928d76d9afa0f814bce90f0
davonyx1 <104191705+davonyx1@users.noreply.github.com>
2022-07-21 13:54:59 +0200

    end render

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,5 +20,20 @@ class ArticleController extends AbstractController
             'article' => $article
         ]);
 
+    }
+    /**
+     * @Route("/voir-articles/{alias}", name="show_articles_from_category", methods={"GET"})
+     */
+    public function showArticleFromCategory(Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Atricle::class)
+        ->findBy([
+            'category' => $category->getId(),
+            'deletedAt' => null        
+        ]);
+            return $this->render("article/show_articles_from_category.html.twig", [
+                'articles' => $articles,
+                'category' => $category
+            ]);
     }
 }
--- src/Controller/RenderController.php
+++ src/Controller/RenderController.php
@@ -2,11 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RenderController extends AbstractController
 {
+    public function renderCategoriesInNav(EntityManagerInterface $entityManager): Response
+    {
+        $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
 
+        return $this->render('rendered/categories_in_nav.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function renderCategoriesInFooter(EntityManagerInterface $entityManager): Response
+    {
+        $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
+
+        return $this ->render('rendered/categories_in_footer.html.twig', [
+            'categories' => $categories
+        ]);
+    }
 }
+
